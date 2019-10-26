b52dc354205977ae38932f9267d8b619f91b4a05
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-26 18:16:02 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,7 +36,7 @@ class ApplicationController < Sinatra::Base
   end 
   
   # delete 
-  delete '/recipes/:id/delete' do 
+  delete '/recipes/:id' do 
     @recipe = Recipe.find_by_id(params[:id])
     @recipe.delete 
     redirect to '/recipes'
