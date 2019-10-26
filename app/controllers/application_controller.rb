ba810c04f757436332142c51651c5f415c97587c
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-26 17:57:52 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,7 @@ class ApplicationController < Sinatra::Base
   
   # show 
   get '/recipes/:id' do 
-    @recipe = Recipe.find 
+    @recipe = Recipe.find_by_id(params[:id])
     erb :show 
   end 
   
