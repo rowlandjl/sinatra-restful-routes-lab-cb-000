0355c6862873e8a734cd3d6d065f3950899a2243
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-26 18:23:40 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,6 +34,9 @@ class ApplicationController < Sinatra::Base
   
   patch '/recipes/:id' do 
     @recipe = Recipe.find_by_id(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
   end 
   
   # delete 
